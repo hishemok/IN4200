0741483d1bad6cbeed53f79c1fc54a3f0cb8ff4c
Hishem <hishemok@uio.no>
2025-05-06 10:28:12 +0200

    update

--- Oblig2/MPI/GS_2chunks_mpi.c
+++ Oblig2/MPI/GS_2chunks_mpi.c
@@ -8,6 +8,7 @@ void GS_iteration_2_chunks_mpi(int my_rank, int kmax, int my_jmax, int imax, dou
     int destination = (my_rank == 0) ? 1 : 0;
     int source = (my_rank == 0) ? 1 : 0;
     int count = imax;
+    MPI_Request reqs[2];
 
     // first wavefront: only computation on left chunck at level k=1
     if (my_rank == 0){
@@ -31,7 +32,7 @@ void GS_iteration_2_chunks_mpi(int my_rank, int kmax, int my_jmax, int imax, dou
         if (my_rank == 0){
             
             tag = 2;
-            MPI_Recv(my_phi[k-1][my_jmax - 1], count, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Irecv(my_phi[k-1][my_jmax - 1], count, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &reqs[0]);
 
             for (j = 1; j < my_jmax-1; j++){               
                 for (i = 1; i < imax-1; i++){
@@ -41,7 +42,7 @@ void GS_iteration_2_chunks_mpi(int my_rank, int kmax, int my_jmax, int imax, dou
                 }
             }
             tag = 3;
-            MPI_Send(my_phi[k][my_jmax - 2], count, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
+            MPI_Isend(my_phi[k][my_jmax - 2], count, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD,&reqs[1]);
              
         }        
         if (my_rank == 1){
@@ -54,12 +55,12 @@ void GS_iteration_2_chunks_mpi(int my_rank, int kmax, int my_jmax, int imax, dou
                 }
             }
             tag = 2;            
-            MPI_Send(my_phi[k-1][1], count, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
+            MPI_Isend(my_phi[k-1][1], count, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD,&reqs[1]);
 
             tag = 3;
-            MPI_Recv(my_phi[k][0], count, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Irecv(my_phi[k][0], count, MPI_DOUBLE, source, tag, MPI_COMM_WORLD,&reqs[0]);
         }
-
+        MPI_Waitall(2, reqs, MPI_STATUS_IGNORE);
     }
 
     // Last wavefront: only computation on right chunk at level k=kmax-2
